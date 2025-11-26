c5b9801630c4e113415e28bf1b883ca413550e37
qowjdals <qowjsals2@gmail.com>
2025-11-26 17:14:26 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const app = express();
-const cors = require("cors");
 
 app.set('view engine', 'ejs');
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -16,13 +13,6 @@ app.get('/search', (req, res) => {
     const v1 = req.query.value1;
     const v2 = req.query.value2;
     res.render('get_result', { v1, v2 });
-});
-
-app.post('/submit-form', (req, res) => {
-    const v1 = Number(req.body.value1);
-    const v2 = Number(req.body.value2);
-    const r1 = v1 % v2;
-    res.render('form_result', { v1, v2, r1 });
 });
 
 app.listen(8080, () => {
